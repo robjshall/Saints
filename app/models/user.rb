572c2b8738786728469f53ee2cae6f493fe6e491
Rob Hall <robjshall@outlook.com>
2013-07-23 14:05:47 +0100

    Basic Gainly Website 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, #:recoverable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable #:recoverable
   # attr_accessible :title, :body
 
   attr_accessible :remember_me, :email, :password, :password_confirmation, :name
