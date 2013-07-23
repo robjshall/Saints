0aebc70a56f146d5a8f660af7f80fb1814f65b83
Rob Hall <robjshall@outlook.com>
2013-07-23 13:56:49 +0100

    Basic Gainly website

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
 
 gem 'sqlite3', :group => [:development, :test]
 group :production do
@@ -13,6 +15,7 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.3.2.1'
 end
 
 
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, #:recoverable
+  # attr_accessible :title, :body
+
+  attr_accessible :remember_me, :email, :password, :password_confirmation, :name
+end
