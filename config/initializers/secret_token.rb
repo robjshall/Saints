75f39f54ee874463df9b8058d92c6f86aa777cc6
Rob Hall <robjshall@outlook.com>
2013-07-23 10:14:03 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gainly2::Application.config.secret_token = '77ac143e2685a4d29e64081f818b653da2516ed9e36926ee13e0c6cea46ea2c41f73f5c54726cb6a66e364605f6427a8735ecbc49ae1be3ea3e648fde3774ee9'
